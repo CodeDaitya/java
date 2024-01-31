c0394958bf41fce6702ead384191847bd67bc301
Deepak Maurya <satyasaadhak.aham@gmail.com>
2024-01-31 22:59:12 +0530

    Complete Hollow Diamond Pattern
         * * * * *
         * *   * *
         *        *
         * *   * *
         * * * * *

--- abhyas/abhyas/src/Assignment1/HollowDiamondPattern.java
+++ abhyas/abhyas/src/Assignment1/HollowDiamondPattern.java
@@ -5,7 +5,7 @@ package Assignment1;
 import java.util.Scanner;
 
 /**
- * incomplete
+ * 
  */
 public class HollowDiamondPattern {
 
@@ -15,29 +15,43 @@ public class HollowDiamondPattern {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		int n = input.nextInt();
+		int num = input.nextInt();
 		input.close();
 		
-		if (n <= 0 || n >= 10 && n%2 == 1) {
+		if (num <= 0 || num >= 10 && num%2 == 0) {
 			return;
 		}
 		
-		for (int i = 1; i <= n; ++i) {
-			if (i <= 1 + n/2) {
-				for (int j = 1; j <= (n - i)/2 + 1 ; ++j) {
+		int n = num / 2 + 1;
+		for (int i = 1; i <= 2*n - 1; ++i) {
+			if (i <= n) {
+				for (int j = n - i + 1; j >= 1; --j) {
 					System.out.print("*\t");
-					if (i == 1 + n/2) {
-						++j;
-					}
 				}
-				for (int j = 1; j <= 2*(i - 1) - 1; ++j) {
+				for (int j = 1; j <= 2*i - 3; ++j) {
 					System.out.print("\t");
 				}
-				for (int j = 1; j <= (n - i)/2 + 1 ; ++j) {
+				for (int j = n - i + 1; j >= 1; --j) {
+					if (i == 1 && j == 1) {
+						continue;
+					}
+					
 					System.out.print("*\t");
 				}
 			} else {
-				
+				for (int j = n + 1; j <= i + 1; ++j) {
+					System.out.print("*\t");
+				}
+				for (int j = 1; j <= 2*(2*n - i) - 3; ++j) {
+					System.out.print("\t");
+				}
+				for (int j = n + 1; j <= i + 1; ++j) {
+					if (i == 2*n - 1 && j == i + 1) {
+						continue;
+					}
+					
+					System.out.print("*\t");
+				}
 			}
 			
 			System.out.println();
