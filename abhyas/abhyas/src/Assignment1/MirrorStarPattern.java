6289e9692f8e71d05a31e84518a55e2fd9c714d3
Deepak Maurya <satyasaadhak.aham@gmail.com>
2024-01-31 23:00:42 +0530

    Implement Mirror Star Pattern
          *
        * * *
      * * * * *
        * * *
          *

--- abhyas/abhyas/src/Assignment1/MirrorStarPattern.java
+++ abhyas/abhyas/src/Assignment1/MirrorStarPattern.java
@@ -3,6 +3,8 @@
  */
 package Assignment1;
 
+import java.util.Scanner;
+
 /**
  * Assignment Patter 5
  */
@@ -13,6 +15,39 @@ public class MirrorStarPattern {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		input.close();
 		
+		if (n <= 0 || n >= 10 && n % 2 == 0) {
+			return;
+		}
+		
+		n = n / 2 + 1;
+		for (int i = 1; i <= 2*n - 1; ++i) {
+			if (i <= n) {
+				for (int j = n - i; j >= 1; --j) {
+					System.out.print("\t");
+				}
+				for (int j = 1; j <= i; ++j) {
+					System.out.print("*\t");
+				}
+				for (int j = 2; j <= i; ++j) {
+					System.out.print("*\t");
+				}
+			} else {
+				for (int j = 1; j <= i - n; ++j) {
+					System.out.print("\t");
+				}
+				for (int j = 1; j <= 2*n - i; ++j) {
+					System.out.print("*\t");
+				}
+				for (int j = 1; j <= 2*n - i - 1; ++j) {
+					System.out.print("*\t");
+				}
+			}
+			
+			System.out.println();
+		}
 	}
 }
